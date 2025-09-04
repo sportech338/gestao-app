34fdeb615a711735407e59c73ae1721c74dfb54b
sportech338 <sportechofc01@gmail.com>
2025-09-03 22:44:48 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -115,7 +115,7 @@ def funnel_fig(labels, values, title=None):
             textinfo="value",
             textposition="inside",
             texttemplate="<b>%{value}</b>",  # deixa o número em negrito
-            textfont=dict(size=21),
+            textfont=dict(size=23),
             opacity=0.95,
             connector={"line": {"dash": "dot", "width": 1}},
         )
