19c0e17534189f456758d1144f03b9e8823bd49d
sportech338 <sportechofc01@gmail.com>
2025-09-12 12:48:08 -0300

    Create constants.py

---
+++ config/constants.py
@@ -0,0 +1,11 @@
+from zoneinfo import ZoneInfo
+
+APP_TZ = ZoneInfo("America/Sao_Paulo")
+ATTR_KEYS = ["7d_click", "1d_view"]
+PRODUTOS = ["Flexlive", "KneePro", "NasalFlex", "Meniscus"]
+HOUR_BREAKDOWN = "hourly_stats_aggregated_by_advertiser_time_zone"
+
+PRESETS = [
+    "Hoje","Ontem","Últimos 7 dias","Últimos 14 dias",
+    "Últimos 30 dias","Últimos 90 dias","Esta semana","Este mês","Máximo","Personalizado"
+]
