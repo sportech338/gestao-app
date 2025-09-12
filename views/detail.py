5a6f7e9d42e29ba4ef402ba82130c4d37a61beaa
sportech338 <sportechofc01@gmail.com>
2025-09-12 13:13:14 -0300

    Create detail.py

---
+++ views/detail.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def render_detail_tab(act_id, token, api_version, level, since, until):
+    st.info("Aba de detalhamento pronta para receber seu conteúdo.")
+    st.caption("Cole aqui os breakdowns (idade, gênero, país, plataforma/posicionamento) e a seção 'Populares'.")
