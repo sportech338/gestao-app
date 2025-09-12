abb1ca00533d2cf9a1b6b745ceb3d7e6d105cef5
sportech338 <sportechofc01@gmail.com>
2025-09-12 13:12:58 -0300

    Create daypart.py

---
+++ views/daypart.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def render_daypart_tab(act_id, token, api_version, since, until):
+    st.info("Aba de horários pronta para receber seu conteúdo.")
+    st.caption("Cole aqui os gráficos de heatmap/linhas/tabelas por hora do seu código original.")
