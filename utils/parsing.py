5b691ded3b1f25768999ed1bd3b97dd586709dff
sportech338 <sportechofc01@gmail.com>
2025-09-22 10:45:11 -0300

    Delete utils directory

--- utils/parsing.py
+++
@@ -1,7 +0,0 @@
-def parse_hour_bucket(h):
-    if h is None: return None
-    try:
-        s=str(h).strip()
-        val=int(s.split(":")[0]) if ":" in s else int(float(s))
-        return max(0, min(23, val))
-    except: return None
