b81640433e9773d5aa53a94064610afbaf1b7352
sportech338 <sportechofc01@gmail.com>
2025-10-24 14:39:12 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2565,7 +2565,7 @@ if menu == "📊 Dashboard – Tráfego Pago":
                     col1, col2, col3 = st.columns(3)
                     with col1:
                         st.markdown(f"""
-                        <div style="background-color:#e05363;padding:18px;border-radius:12px;
+                        <div style="background-color:#c93e3e;padding:18px;border-radius:12px;
                                     border:1px solid #FCA5A5;color:#000000;opacity:1;filter:none;">
                             <h5 style="color:#000000 !important;font-weight:700;font-size:15px;
                                        margin:0 0 6px 0;opacity:1;filter:none;">
@@ -2582,7 +2582,7 @@ if menu == "📊 Dashboard – Tráfego Pago":
 
                     with col2:
                         st.markdown(f"""
-                        <div style="background-color:#e05363;padding:18px;border-radius:12px;
+                        <div style="background-color:#c93e3e;padding:18px;border-radius:12px;
                                     border:1px solid #FACC15;color:#000000;opacity:1;filter:none;">
                             <h5 style="color:#000000 !important;font-weight:700;font-size:15px;
                                        margin:0 0 6px 0;opacity:1;filter:none;">
@@ -2598,7 +2598,7 @@ if menu == "📊 Dashboard – Tráfego Pago":
 
                     with col3:
                         st.markdown(f"""
-                        <div style="background-color:#e05363;padding:18px;border-radius:12px;
+                        <div style="background-color:#c93e3e;padding:18px;border-radius:12px;
                                     border:1px solid #F87171;color:#000000;opacity:1;filter:none;">
                             <h5 style="color:#000000 !important;font-weight:700;font-size:15px;
                                        margin:0 0 6px 0;opacity:1;filter:none;">
