93549af44730d66ab7b2087b27db4b3665e03f84
sportech338 <sportechofc01@gmail.com>
2025-10-27 17:05:34 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3172,7 +3172,7 @@ if menu == "📦 Dashboard – Logística":
     """, unsafe_allow_html=True)
 
     colunas = ["created_at", order_col, "customer_name", "quantity", "product_title",
-               "variant_title", "fulfillment_status"]
+               "variant_title", "forma_entrega"]
     colunas = [c for c in colunas if c in df.columns]
     tabela = df[colunas].sort_values("created_at", ascending=False).copy()
 
