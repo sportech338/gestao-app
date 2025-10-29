36c1e67f7069b9850798036352e3f92e4ac550ea
sportech338 <sportechofc01@gmail.com>
2025-10-29 19:59:03 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3742,8 +3742,9 @@ if menu == "📦 Dashboard – Logística":
 
         if st.button("💾 Salvar alterações na planilha"):
             atualizar_planilha_custos(edit_df)
+            st.cache_data.clear()
+            st.rerun()
 
-    
     # =====================================================
     # 🚚 ABA 3 — ENTREGAS
     # =====================================================
